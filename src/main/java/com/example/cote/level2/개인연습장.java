f66c9acb2660523958a9145375e6b07c5b9cb508
이상범 <83106958+lsangbum@users.noreply.github.com>
2023-10-23 13:22:38 +0900

    231023

---
+++ src/main/java/com/example/cote/level2/개인연습장.java
@@ -0,0 +1,20 @@
+package com.example.cote.level2;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class 개인연습장 {
+    public static void main(String[] args) throws IOException {
+        Map y = new HashMap();
+        List x = new ArrayList();
+        x.add(1);
+        x.add("1");
+        x.add(y);
+
+        System.out.println();
+    }
+
+}
--- src/main/java/com/example/cote/level2/집합과맵/회사에있는사람.java
+++ src/main/java/com/example/cote/level2/집합과맵/회사에있는사람.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class 회사에있는사람 {
